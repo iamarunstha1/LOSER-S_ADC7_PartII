06d7852d9c3d8eba6ce25b156d26310bc2ef5582
IshworAmgai <np03a180206@heraldcollege.edu.np>
2020-02-10 16:27:03 +0545

    relations

--- WebApp/faceofnepal/admin.py
+++ WebApp/faceofnepal/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from .models import Freelancer,Tourist
+from .models import Freelancer
+from .models import Visitor
+from .models import Payment
+from .models import Business
+from .models import Administrator
+
+
+
 # Register your models here.
 admin.site.register(Freelancer)
-admin.site.register(Tourist)
+admin.site.register(Visitor)
+admin.site.register(Payment)
+admin.site.register(Business)
+admin.site.register(Administrator)
+
+
+
--- WebApp/faceofnepal/models.py
+++ WebApp/faceofnepal/models.py
@@ -17,10 +17,43 @@ class Freelancer(models.Model):
      return f"My Name is {self.Name}. I am from {self.Address} and my contact no. {self.Phone}"
 
 
-class Tourist(models.Model):
-    name = models.ForeignKey(Freelancer,on_delete=models.CASCADE,related_name="names")
-    address = models.ForeignKey(Freelancer,on_delete=models.CASCADE,related_name="add")
-    phone = models.IntegerField()
+class Payment(models.Model):
+    Cash=models.IntegerField()  
+    Cheque=models.CharField(max_length=30)
 
     # def __str__(self):
-    #     return f"I am {self.name.Name} address  {self.address.Address} and {self.phone}"
+    #     return "{self.Cash,self.Cheque}"
+
+class Visitor(models.Model):
+    Guide=models.ManyToManyField(Freelancer,blank=True,related_name="guide")
+    Visitor_Name=models.CharField(max_length=20)
+    Visitor_Email=models.EmailField(max_length=30)
+    Visitor_Mobile_No=models.IntegerField()
+    Visitor_address=models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.Guide,self.Visitor_Name,self.Visitor_Email,self.Visitor_Mobile_No,self.Visitor_address}"
+
+
+class Business(models.Model):
+    company_name=models.CharField(max_length=20)
+    company_location=models.CharField(max_length=20)
+    package_name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.company_name,self.company_location,self.package_name}"
+
+class Administrator(models.Model):
+    admin=models.ForeignKey(Freelancer,on_delete=models.CASCADE,related_name='admin' )
+    Guide=models.OneToOneField(Freelancer,  on_delete = models.CASCADE, primary_key = False,related_name='admin_guide')
+    Username=models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.Username,self.admin,self.Guide}" 
+
+
+
+
+
+
+
+
